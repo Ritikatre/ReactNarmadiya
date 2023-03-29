0915b01d57ed28eeecae9ae37adcde85d0a30250
Ritik <ritikatre27@gmail.com>
2023-03-29 13:05:05 +0530

    new changes in censuslist

--- Backend/db.js
+++ Backend/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 // "mongodb://localhost:27017/" <- Connection String available at mongo Compass
-const mongoUrI = "mongodb://localhost:27017/nduser" // nduser is database name 
- 
-const connectToMongo = ()=>{
+//const mongoUrI = "mongodb+srv://ritikatre:Rtk2Mongodb@narmadiya.5babnpx.mongodb.net/test" // nduser is database name 
+const mongoUrI = "mongodb://localhost:27017/nduser" // nduser is database name  
+const connectToMongo = ()=>{    
 
     mongoose.connect(mongoUrI, ()=> {
 
